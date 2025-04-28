054e94c43b5817ba02b40a2334ca2c1d055db7b5
unknown <gsemily22@gmail.com>
2025-04-28 09:23:00 +0900

    first commit

--- src/두_수의_차.java
+++ src/두_수의_차.java
@@ -5,7 +5,7 @@
 //-50000 ≤ num1 ≤ 50000
 //-50000 ≤ num2 ≤ 50000*/
 
-public class Solution {
+public class 두_수의_차 {
     public int solution(int num1, int num2) {
         int answer = 0;
         if((num1 >= -50000 && num1 <= 50000) && (num2 >= -50000 && num2 <= 50000))
