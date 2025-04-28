7f6118b85a14e556565657dfead9d1a03639e716
unknown <gsemily22@gmail.com>
2025-04-28 09:17:01 +0900

    first commit

---
+++ src/Solution.java
@@ -0,0 +1,15 @@
+//문제 설명
+//정수 num1과 num2가 주어질 때, num1에서 num2를 뺀 값을 return하도록 soltuion 함수를 완성해주세요.
+
+//제한사항
+//-50000 ≤ num1 ≤ 50000
+//-50000 ≤ num2 ≤ 50000*/
+
+public class Solution {
+    public int solution(int num1, int num2) {
+        int answer = 0;
+        if((num1 >= -50000 && num1 <= 50000) && (num2 >= -50000 && num2 <= 50000))
+            answer = num1 - num2;
+        return answer;
+    }
+}
